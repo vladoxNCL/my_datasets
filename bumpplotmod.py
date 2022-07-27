bcf619eb98c7ec79c58d19c4aed986dc57025b0e
vladoxNCL <33488410+vladoxNCL@users.noreply.github.com>
2022-07-27 18:22:02 +0100

    bump plots

---
+++ bumpplotmod.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits import axisartist
+
+def bumpchart(df, show_rank_axis=True, rank_axis_distance=1.1, 
+              ax=None, scatter=False, holes=False,
+              line_args={}, scatter_args={}, hole_args={}):
+    if ax is None:
+        left_yaxis= plt.gca()
+    else:
+        left_yaxis = ax
+
+    # Added by me
+    def make_patch_spines_invisible(ax):
+        ax.set_frame_on(True)
+        ax.patch.set_visible(False)
+        for sp in ax.spines.values():
+            sp.set_visible(False)
+    
+    # Creating the right axis.
+    right_yaxis = left_yaxis.twinx()
+    
+    axes = [left_yaxis, right_yaxis]
+    
+    # Creating the far right axis if show_rank_axis is True
+    if show_rank_axis:
+        far_right_yaxis = left_yaxis.twinx()
+        axes.append(far_right_yaxis)
+        # Added by me
+        far_left = left_yaxis.twinx()
+        axes.append(far_left)
+    
+    for col in df.columns:
+        y = df[col]
+        x = df.index.values
+        # Plotting blank points on the right axis/axes 
+        # so that they line up with the left axis.
+        for axis in axes[1:]:
+            axis.plot(x, y, alpha=0)
+
+        left_yaxis.plot(x, y, **line_args, solid_capstyle='round')
+        
+        # Adding scatter plots
+        if scatter:
+            left_yaxis.scatter(x, y, **scatter_args)
+            
+            #Adding see-through holes
+            if holes:
+                bg_color = left_yaxis.get_facecolor()
+                left_yaxis.scatter(x, y, color=bg_color, **hole_args)
+
+    # Number of lines
+    lines = len(df.columns)
+
+    y_ticks = [*range(1, lines + 1)]
+    
+    # Configuring the axes so that they line up well.
+    for axis in axes:
+        axis.invert_yaxis()
+        axis.set_yticks(y_ticks)
+        axis.set_ylim((lines + 0.5, 0.5))
+    
+    # Sorting the labels to match the ranks.
+    left_labels = df.iloc[0].sort_values().index
+    right_labels = df.iloc[-1].sort_values().index
+    
+    left_yaxis.set_yticklabels(left_labels)
+    right_yaxis.set_yticklabels(right_labels)
+    
+    # Setting the position of the far right axis so that it doesn't overlap with the right axis
+    if show_rank_axis:
+        far_right_yaxis.spines["right"].set_position(("axes", rank_axis_distance))
+        # Added by me
+        far_left.spines["left"].set_position(("axes", 1-rank_axis_distance))
+        make_patch_spines_invisible(far_left)
+        far_left.spines["left"].set_visible(True)
+        far_left.yaxis.set_label_position('left')
+        far_left.yaxis.set_ticks_position('left')
+    
+    return axes
